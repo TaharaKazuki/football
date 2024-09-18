54de25e68d9d259f280a736ea51ac049489c09ca
Tahara Kazuki <>
2024-09-18 17:09:17 +0900

    update

--- app/components/searchbar/SearchBarForm.tsx
+++ app/components/searchbar/SearchBarForm.tsx
@@ -2,7 +2,14 @@
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import {
+  ChangeEvent,
+  useEffect,
+  useRef,
+  useState,
+  useMemo,
+  useCallback,
+} from 'react';
 
 import { Team } from '@/types';
 
@@ -14,64 +21,65 @@ const SearchBarForm = ({ teamsData }: SearchBarFormProps) => {
   const [searchTeam, setSearchTeam] = useState<string>('');
   const [focusedIndex, setFocusedIndex] = useState<number>(-1);
   const [showFilteredBox, setShowFilteredBox] = useState<boolean>(false);
-
   const router = useRouter();
+  const teamListRef = useRef<HTMLDivElement>(null);
 
-  const filteredTeams = teamsData.filter((team) =>
-    team.team.name.toLocaleLowerCase().includes(searchTeam.toLocaleLowerCase())
+  const filteredTeams = useMemo(
+    () =>
+      teamsData.filter((team) =>
+        team.team.name.toLowerCase().includes(searchTeam.toLowerCase())
+      ),
+    [searchTeam, teamsData]
   );
 
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setSearchTeam(e.target.value);
     setFocusedIndex(-1);
     setShowFilteredBox(true);
-  };
+  }, []);
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'ArrowDown') {
-      let length = 0;
-      if (filteredTeams.length > 10) {
-        length = 10;
-      } else {
-        length = filteredTeams.length;
-      }
-      console.log(focusedIndex);
-      setFocusedIndex((prevIndex) =>
-        prevIndex < length - 1 ? prevIndex + 1 : prevIndex
-      );
-    } else if (event.key === 'ArrowUp') {
-      event.preventDefault();
-      setFocusedIndex((prevIndex) =>
-        prevIndex > 0 ? prevIndex - 1 : prevIndex
-      );
-    } else if (event.key === 'Enter') {
-      if (focusedIndex !== -1) {
+  const handleKeyDown = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      const length = Math.min(filteredTeams.length, 10);
+
+      if (event.key === 'ArrowDown') {
+        setFocusedIndex((prevIndex) =>
+          prevIndex < length - 1 ? prevIndex + 1 : prevIndex
+        );
+      } else if (event.key === 'ArrowUp') {
+        event.preventDefault();
+        setFocusedIndex((prevIndex) =>
+          prevIndex > 0 ? prevIndex - 1 : prevIndex
+        );
+      } else if (event.key === 'Enter' && focusedIndex !== -1) {
         const teamId = filteredTeams[focusedIndex].team.id;
         router.push(`/team/${teamId}`);
         setSearchTeam('');
       }
-    }
-  };
+    },
+    [filteredTeams, focusedIndex, router]
+  );
 
-  const handleTeamItemClick = () => setSearchTeam('');
+  const handleTeamItemClick = useCallback(() => setSearchTeam(''), []);
 
-  const teamListRef = useRef<HTMLDivElement>(null);
-
-  const handleOutsideClick = (event: MouseEvent) => {
+  const handleOutsideClick = useCallback((event: MouseEvent) => {
     if (
       teamListRef.current &&
       !teamListRef.current.contains(event.target as Node)
     ) {
       setShowFilteredBox(false);
     }
-  };
+  }, []);
 
   useEffect(() => {
     document.addEventListener('click', handleOutsideClick);
     return () => {
       document.removeEventListener('click', handleOutsideClick);
     };
-  }, []);
+  }, [handleOutsideClick]);
+
+  const shouldShowFilteredBox =
+    searchTeam && filteredTeams.length > 0 && showFilteredBox;
 
   return (
     <div className="relative flex w-full max-w-lg items-center justify-center">
@@ -86,26 +94,26 @@ const SearchBarForm = ({ teamsData }: SearchBarFormProps) => {
                 text-neutral-100/80 outline-none
                 transition-all duration-300 placeholder:text-neutral-100/30 hover:border-yellow-200 focus:border-yellow-200 focus:from-yellow-200/40"
       />
-      {searchTeam && filteredTeams.length > 0 && showFilteredBox ? (
+      {shouldShowFilteredBox && (
         <div
           ref={teamListRef}
-          className="absolute left-2 top-full z-20 flex w-full
-                        max-w-md flex-col bg-black/80"
+          className="absolute top-full z-20 flex w-full
+                        max-w-full flex-col bg-black/80"
         >
           {filteredTeams.slice(0, 10).map((standing, i) => (
             <Link
               href={`/team/${standing.team.id}`}
               key={standing.team.id}
-              className={`p-2 text-neutral-100 ${
-                i === focusedIndex ? 'bg-neutral-100/40' : ''
+              className={`p-2 text-neutral-100 hover:bg-yellow-100/40 ${
+                i === focusedIndex ? 'bg-yellow-100/40' : ''
               }`}
-              onClick={() => handleTeamItemClick()}
+              onClick={handleTeamItemClick}
             >
               {standing.team.name}
             </Link>
           ))}
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
